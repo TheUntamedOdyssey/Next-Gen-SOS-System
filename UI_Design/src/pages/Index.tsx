0f2294ae31dfd83fa9041295021bbfcb10a39c88
Sneha Ghosh (Technical_Isopod_075) <153223798+SnehaghoshBarsha444@users.noreply.github.com>
2025-03-16 21:21:24 +0530

    Add files via upload

---
+++ UI_Design/src/pages/Index.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Layout from '../components/Layout';
+import EmergencyDashboard from '../components/EmergencyDashboard';
+
+const Index: React.FC = () => {
+  return (
+    <Layout>
+      <EmergencyDashboard />
+    </Layout>
+  );
+};
+
+export default Index;
